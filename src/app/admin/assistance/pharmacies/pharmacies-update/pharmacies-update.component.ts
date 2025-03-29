dfe87336ef0bbea0545c8f915addd2a42e2d3f1f
devnatera <devnatera@gmail.com>
2025-03-29 16:38:28 -0500

    Desarrollo CRUD farmacias

--- src/app/admin/assistance/pharmacies/pharmacies-update/pharmacies-update.component.ts
+++ src/app/admin/assistance/pharmacies/pharmacies-update/pharmacies-update.component.ts
@@ -1,8 +1,276 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import ButtonComponent from '@components/button/button.component';
+import { DynamicFormComponent } from '@components/dynamic-form/dynamic-form.component';
+import { Pharmacies } from '@interfaces/assistance/pharmacies.interfaces';
+import { DynamicForm } from '@interfaces/util/dynamic-form.interface';
+import { PharmaciesService } from '@services/assistance/pharmacies.service';
+import { AuthService } from '@services/auth/auth.service';
+import { LoadingService } from '@services/util/loading.service';
+import { NotificationService } from '@services/util/notificacion.service';
+import { UtilidadesService } from '@services/util/utilidades.service';
+import { CardModule } from 'primeng/card';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'app-pharmacies-update',
-  imports: [],
+  imports: [DynamicFormComponent, CardModule, ButtonComponent],
   templateUrl: './pharmacies-update.component.html',
 })
-export default class PharmaciesUpdateComponent {}
+export default class PharmaciesUpdateComponent {
+
+  @ViewChild(DynamicFormComponent) dynamicFormComponent!: DynamicFormComponent;
+    private readonly subscription: Subscription[] = [];
+    private farmaciaId = '';
+
+    formBtnConfig = [
+      {
+        label: 'Actualizar',
+        icon: 'bx bx-refresh bx-sm',
+        visible: true,
+        width: 'w-full',
+        appearance: 'base',
+        color: 'primary',
+        action: 'actualizar',
+        disabled: false,
+        loading: false,
+      },
+    ];
+
+    formConfig: DynamicForm[] = [
+        {
+          type: 'select',
+          name: 'utilidad_tipo_identificacion_id',
+          label: 'Tipo de identificacion',
+          on_label: 'utilidad_tipo_identificacion_id',
+          placeholder: '',
+          filter: true,
+          filterBy: 'nombre',
+          showClear: true,
+          options: [],
+          selectedItems: [],
+          validators: {
+            required: true,
+          },
+          column: 'col-12 md:col-4 lg:col-3',
+        },
+        {
+          type: 'number',
+          icon: 'id-card',
+          name: 'identificacion',
+          label: 'identificacion',
+          on_label: 'identificacion',
+          placeholder: '',
+          validators: {
+            required: true,
+            minLength: 3,
+            maxLength: 20,
+          },
+          column: 'col-12 md:col-4 lg:col-3',
+        },
+        {
+          type: 'text',
+          icon: 'user',
+          name: 'razon_social',
+          label: 'Nombre',
+          on_label: 'razon_social',
+          placeholder: '',
+          validators: {
+            required: true,
+            minLength: 3,
+            maxLength: 150,
+          },
+          column: 'col-12 md:col-4 lg:col-6',
+        },
+        {
+          type: 'text',
+          icon: 'location-plus',
+          name: 'direccion',
+          label: 'Direccion',
+          on_label: 'direccion',
+          placeholder: '',
+          validators: {
+            required: true,
+            minLength: 6,
+          },
+          column: 'col-12 md:col-4 lg:col-4',
+        },
+        {
+          type: 'select',
+          name: 'utilidad_departamento_id',
+          label: 'Departamento',
+          on_label: 'utilidad_departamento_id',
+          placeholder: '',
+          filter: true,
+          filterBy: 'nombre',
+          showClear: true,
+          options: [],
+          selectedItems: [],
+          validators: {
+            required: true,
+          },
+          column: 'col-12 md:col-4 lg:col-4',
+          onChange: (event: any) =>
+            this.getListadoCiudadesPorDepartamento(event.value),
+        },
+        {
+          type: 'select',
+          name: 'utilidad_ciudad_id',
+          label: 'Ciudad',
+          on_label: 'utilidad_ciudad_id',
+          placeholder: '',
+          filter: true,
+          filterBy: 'nombre',
+          showClear: true,
+          options: [],
+          selectedItems: [],
+          validators: {
+            required: true,
+          },
+          column: 'col-12 md:col-4 lg:col-4',
+        },
+        {
+          type: 'tel',
+          icon: 'phone',
+          name: 'telefonos',
+          label: 'Contactos',
+          on_label: 'telefonos',
+          placeholder: '',
+          validators: {
+            required: true,
+            minLength: 9,
+            maxLength: 14,
+            pattern: '^(\\+\\d{1,3}[- ]?)?\\d{10}$',
+          },
+          column: 'col-12 md:col-4 lg:col-4',
+        },
+        {
+          type: 'checkbox',
+          name: 'estado',
+          label: 'Estado',
+          on_label: 'estado',
+          column: 'col-12 md:col-4 lg:col-4',
+        },
+        {
+          type: 'textarea',
+          name: 'descripcion',
+          label: 'Descripción',
+          on_label: 'descripcion',
+          placeholder: '',
+          validators: {
+            required: false,
+          },
+          column: 'col-12',
+        },
+      ];
+    
+      constructor(
+        private readonly _utilidadesService: UtilidadesService,
+        private readonly _notificationService: NotificationService,
+        private readonly _pharmaciesService: PharmaciesService,
+        private readonly route: ActivatedRoute,
+        private readonly _authService: AuthService,
+        private readonly _loadingService: LoadingService,
+        private readonly _router: Router
+      ) {
+        this.farmaciaId = this.route.snapshot.paramMap.get('id')!;
+      }
+    
+      goToReturnUrl(): void {
+        this._router.navigate(['admin/assistance/pharmacies']);
+      }
+    
+      getListadoTipoIdentificacion(): void {
+        this.subscription.push(
+          this._utilidadesService
+            .getListadoTipoIdentificacion({ estados: ['activo'] })
+            .subscribe((response) => {
+              this.formConfig.find(
+                (field) => field.name === 'utilidad_tipo_identificacion_id'
+              )!.options = response.data;
+            })
+        );
+      }
+    
+      getListadoDepartamentos(): void {
+        this.subscription.push(
+          this._utilidadesService
+            .getListadoDepartamentos({ estados: ['activo'] })
+            .subscribe((response) => {
+              this.formConfig.find(
+                (field) => field.name === 'utilidad_departamento_id'
+              )!.options = response.data;
+            })
+        );
+      }
+    
+      getListadoCiudadesPorDepartamento(departmentId: string): void {
+        if (departmentId) {
+          this.subscription.push(
+            this._utilidadesService
+              .getListadoCiudadesPorDepartamento({
+                estados: ['activo'],
+                utilidad_departamento_id: departmentId,
+              })
+              .subscribe((response) => {
+                this.formConfig.find(
+                  (field) => field.name === 'utilidad_ciudad_id'
+                )!.options = response.data;
+              })
+          );
+        }
+      }
+    
+      getById(): void {
+        if (this.farmaciaId) {
+          this.subscription.push(
+            this._pharmaciesService
+              .getById({
+                estados: ['activo'],
+                id: this.farmaciaId,
+                ma_entidad_id: this._authService.getEntityStorage.id.toString(),
+              })
+              .subscribe((farmacia) => {
+                this.dynamicFormComponent.setFormData({
+                  ...farmacia.data,
+                  estado: farmacia.data.estado == 'activo',
+                });
+                this.getListadoCiudadesPorDepartamento(
+                  farmacia.data.utilidad_departamento_id
+                );
+              })
+          );
+        }
+      }
+    
+      put(data: any): void {
+        const farmacia: Pharmacies = {
+          ...data.form,
+          estado: data.form.estado ? 'activo' : 'inactivo',
+          ma_entidad_id: this._authService.getEntityStorage.id.toString(),
+        };
+        this.subscription.push(
+          this._pharmaciesService.put(this.farmaciaId, farmacia).subscribe((res) => {
+            this._notificationService.showSuccess(res.message);
+            this.goToReturnUrl();
+          })
+        );
+      }
+    
+      ngOnInit(): void {
+        this._loadingService.loading$.subscribe((loading) => {
+          this.formBtnConfig.find((btn) => btn.label === 'Actualizar')!.loading =
+            loading;
+        });
+        this.getListadoTipoIdentificacion();
+        this.getListadoDepartamentos();
+      }
+    
+      ngAfterViewInit() {
+        this.getById();
+      }
+    
+      ngOnDestroy(): void {
+        this.subscription.forEach((s) => s.unsubscribe());
+      }
+}
---
+++ src/app/core/interfaces/assistance/pharmacies.interfaces.ts
@@ -0,0 +1,13 @@
+export interface Pharmacies {
+  ma_entidad_id: string;
+  utilidad_tipo_identificacion_id: string;
+  identificacion: string;
+  razon_social: string;
+  slug: string;
+  utilidad_departamento_id: string;
+  utilidad_ciudad_id: string;
+  direccion: string;
+  telefonos: string;
+  descripcion: string;
+  estado: string;
+}
